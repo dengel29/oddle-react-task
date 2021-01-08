177b63277920cd8d0e49777c987c196c2df7907f
Dan Engel <dengel29@gmail.com>
2021-01-09 00:51:01 +0800

    added some todos to the userprofile so i dont forget

--- src/components/UserProfile/UserProfile.tsx
+++ src/components/UserProfile/UserProfile.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
 
 const UserProfile = (props: any) => {
-  console.log(props)
   let user = props.location.state.user
+  console.log(props)
+  /**
+   * TODO
+   * [] make requests to the user repositories api, folowers api, and following api
+   * [] make it look pretty
+   * [] add a back button
+   * [] make sure the historyAPI is working(?)
+   * [] create a check that, if the route is accessed without being "navigated" 
+   * to from within the app, that it will fetch the correct data anyways
+   */
 
   return(
     <React.Fragment>
